78719f9116430b808eeca379f277443153c04a75
Yudiel <yhonnierrg@gmail.com>
2023-11-05 19:10:10 -0500

    Initial Commit

---
+++ Yuber/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Yuber
+//
+//  Created by Honnier on 5/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yuber/YuberApp.swift
@@ -0,0 +1,17 @@
+//
+//  YuberApp.swift
+//  Yuber
+//
+//  Created by Honnier on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct YuberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
